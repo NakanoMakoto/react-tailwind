d82a47c439af30ee68f63ecdd1d4acd768490dd1
NakanoMakoto <m.nakano8649@gmail.com>
2019-04-26 01:04:37 +0900

    define header

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,60 @@
-import React from 'react';
+import React from "react";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
-            <div className="flex items-center flex-no-shrink text-white mr-6">
-                <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-                <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+      <header>
+        <nav className="relative select-none bg-teal lg:flex lg:items-stretch w-full">
+          <div className="flex flex-no-shrink items-stretch h-12">
+            <a
+              href="/"
+              className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-teal-dark"
+            >
+              まぶろぐ
+            </a>
+            <button className="block lg:hidden cursor-pointer ml-auto relative w-12 h-12 p-4">
+              <svg
+                className="fill-current text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+              <svg
+                className="fill-current text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+              </svg>
+            </button>
+          </div>
+          <div className="lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow">
+            <div className="lg:flex lg:items-stretch lg:justify-end ml-auto">
+              <a
+                href="/"
+                className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-teal-dark"
+              >
+                ABOUT
+              </a>
+              <a
+                href="/"
+                className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-teal-dark"
+              >
+                ポリシー
+              </a>
+              <a
+                href="/"
+                className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-teal-dark"
+              >
+                問い合わせ
+              </a>
             </div>
-            <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
-                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
-            </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-            <div className="text-sm lg:flex-grow">
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-                Docs
-                </a>
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-                Examples
-                </a>
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
-                Blog
-                </a>
-            </div>
-            <div>
-                <a href="/" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0">Download</a>
-            </div>
-            </div>
+          </div>
         </nav>
+      </header>
     </div>
   );
-}
+};
 
 export default App;
